2e2f1ca375b6ce87bf3c770c8fe17432592a7d03
marvinxu99 <marvinxu99@hotmail.com>
2023-07-03 13:45:19 -0700

    view can be function, class-based, using mixins, generic class-based view

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,6 +3,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 
+### View Using function 
+# urlpatterns = [
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail),
+# ]
+
+
+### View Using class-based, or mixins or generic class-based 
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
